2eb6080fc3b7f29abcddf81cd5a1f6655f1e81de
Evgeni.ok14 <evgeni.ok@mail.ru>
2022-11-30 13:28:07 +0300

    Создал новый проект Tusk001

---
+++ tusk001/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.Clear();
+Console.Write("Введите первое число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Первое число N равно: " + N);
+Console.Write("Введите второе число M: ");
+int M = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Второе число M равно: " + M);
+int Max = N;
+int Min = M;
+  if (N > M) 
+  {
+     N = Max;
+     Console.WriteLine("наибольшее из двух чисел N и M является число: " + N);
+  }
+  if (M > N)
+  {
+    M = Max;
+    Console.WriteLine("наибольше из двух чисел N и M является число: " + M);
+  }
+      if (N == M)
+      {
+        Console.WriteLine("Оба числа равны: " + N + " = " + M);
+
+      }
