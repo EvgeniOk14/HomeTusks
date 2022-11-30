27980987ead449709088e4860c6a725be2104ef7
Evgeni.ok14 <evgeni.ok@mail.ru>
2022-11-30 18:22:23 +0300

    Добавил новый проект Tusk003

---
+++ tusk003/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+Console.WriteLine("Введите целое число N: ");
+
+  double N = Convert.ToDouble(Console.ReadLine());
+  if (N % 2 == 0) 
+  {
+    Console.WriteLine(N + " это чётное число ");
+  }
+     
+  else
+  {
+    Console.WriteLine(N + " это нечётное число ");
+  }
+  
