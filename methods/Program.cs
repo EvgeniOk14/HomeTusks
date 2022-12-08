8a9c97f3702b0552f1a623be4bca47201022476d
Evgeni.ok14 <evgeni.ok@mail.ru>
2022-12-08 17:36:07 +0300

    added a new project: Methods

--- methods/Program.cs
+++ methods/Program.cs
@@ -1,4 +1,12 @@
-﻿void FillArray (int[] collection)
+﻿void WriteLength()
+{
+    Console.WriteLine("Задайте длину массива n: ");
+    int n = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("длина массива равна:" + n);
+    Console.WriteLine();
+}
+
+void FillArray (int[] collection)
 {
     int length = collection.Length;
     for (int i = 0; i < length; i++)
@@ -18,32 +26,32 @@ void PrintArray(int[] col)
     Console.WriteLine();
 }
      
-void SelectionSort(int[] array)
-{
+ void SelectionSort(int[] array)
+ {
 
-    for (int i = 1; i < array.Length - 1; i++)
-    {
-             int indexForMin = i;
+    for (int i = 0; i < array.Length - 1; i++)
+       {
+         int indexForMin = i;
 
-               for (int j = i +1; j < array.Length; j++)
+         for (int j = i + 1; j < array.Length; j++)
                   {
                       if (array[j] < array[indexForMin])
-                          {
-                             indexForMin = j;
-                          }  
-                    int temp = array[i];
-                    array[i] = array[indexForMin];
-                    array[indexForMin] = temp;
-                          
-                   }
-    }
+                             indexForMin = j;        
+                  }
+
+         int temp = array[i];
+         array[i] = array[indexForMin];
+         array[indexForMin] = temp;
+       }    
+
     Console.WriteLine();
-}
+ }
 
     int [] array = new int[10];
 
+    WriteLength();
     FillArray(array); 
     PrintArray(array);
     SelectionSort(array);
     PrintArray(array);
-    
+   
