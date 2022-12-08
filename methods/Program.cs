d529e6e160de72b86725e0445c80efc14fa7e360
Evgeni.ok14 <evgeni.ok@mail.ru>
2022-12-08 15:37:14 +0300

    added SelectionSort Method

--- methods/Program.cs
+++ methods/Program.cs
@@ -1,32 +1,49 @@
 ﻿void FillArray (int[] collection)
 {
     int length = collection.Length;
-    int index = 0;
-     
-      while (index < length)
+    for (int i = 0; i < length; i++)
       {
-        collection[index] = new Random().Next(1,10);
-        index++;
+        collection[i] = new Random().Next(1,10); 
       }
+      Console.WriteLine();
 }
 
 void PrintArray(int[] col)
 {
   int count = col.Length;
-  int position = 0;
-    
-    while (position < count)
+  for (int i = 0; i < count; i++)
     {
-       Console.WriteLine(col[position]);
-       position++;
+       Console.Write(col[i]);
     }
+    Console.WriteLine();
 }
-    
-    
+     
+void SelectionSort(int[] array)
+{
+
+    for (int i = 1; i < array.Length - 1; i++)
+    {
+             int indexForMin = i;
+
+               for (int j = i +1; j < array.Length; j++)
+                  {
+                      if (array[j] < array[indexForMin])
+                          {
+                             indexForMin = j;
+                          }  
+                    int temp = array[i];
+                    array[i] = array[indexForMin];
+                    array[indexForMin] = temp;
+                          
+                   }
+    }
+    Console.WriteLine();
+}
+
     int [] array = new int[10];
 
-    FillArray(array);
+    FillArray(array); 
     PrintArray(array);
-
-
+    SelectionSort(array);
+    PrintArray(array);
     
